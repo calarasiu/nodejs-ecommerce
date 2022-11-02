38d385f4ef4be05139235b2945eec56a57420895
calarasiue <cornelia.calarasiu@yahoo.it>
2022-11-02 08:13:16 +0000

    add the home screen and products component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Container } from 'react-bootstrap'
-import Footer from "./Footer";
-import Header from "./Header";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 
 
 function App() {
   return (
     <>
       <Header/>
-      <main>
+      <main className="py-3">
         <Container>
 
-          <h1>Welcome to Proshop</h1>
+          <HomeScreen/>
         </Container>
       </main>
       <Footer/>
